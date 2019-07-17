44b9b36b05252aa96db4024381daab68cd629bd4
simon-group <saku_crownheart@yahoo.co.jp>
2019-07-17 17:50:27 +0900

    syusei commit

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -14,7 +14,7 @@ $(document).on('turbolinks:load', function(){
     var member_list = $(".chat-group-users");
 
     function addUser(name,user_id) {
-    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-22'>
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='${user_id}'>
                   <input name='group[user_ids][]' type='hidden' value='${user_id}'>
                   <p class='chat-group-user__name'>${name}</p>
                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
